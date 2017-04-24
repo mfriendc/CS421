fde5933e1ac54d89ce7e8491134ccf79858a96e8
Mfriendc <mfriend@hawaii.edu>
2017-04-23 23:20:46 -1000

    test with views

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express 	  =     require('express')
+var exphbs        =     require('express-handlebars')
 var app 		  =     express()
 var bodyParser	  =     require('body-parser')
 var datetime      =     require('node-datetime')
 const query = require("./sqlquery.js")
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 // middle-wares
 app.use(express.static('static'))
@@ -24,7 +28,6 @@ app.get('/itest', (req, res) => {
 })
 
 app.get('/locations', (req, res) => {
-	console.log(res != null && typeof res !== 'undefined')
 	query.getLocations()
 	.then((r) => {
 		console.log(r)
@@ -36,10 +39,60 @@ app.get('/locations', (req, res) => {
 	})
 })
 
+app.get('/routes', (req, res) => {
+	query.getRouteIDs({})
+	.then((r) => {
+		console.log(r)
+		res.set('Content-Type', 'application/json')
+		res.send(r)
+	})
+	.fail((e) => {
+		res.send(e)
+	})
+})
+
+app.get('/hb', (req, res) => {
+	query.getLocations()
+	.then((r) => {
+		console.log(r)
+		console.log("got")
+		res.render('test', {layout:false, locs: r})
+	})
+	.fail((e) => {
+		res.send(e)
+	})
+})
+
+app.get('/rir', (req, res) => {
+	res.send(query.getResultsInRoute())
+})
+
 app.post('/form', (req, res) => {
-	console.log(req)
-	console.log("===========")
 	console.log(req.body)
+})
+
+app.get('/drivera', (req, res) => {
+	query.getDriverNames(true)
+	.then((r) => {
+		console.log(r)
+		console.log("got")
+		res.render('tabletest', {layout:false, table: r})
+	})
+	.fail((e) => {
+		res.send(e)
+	})
+})
+
+app.get('/driver', (req, res) => {
+	query.getDriverNames(false)
+	.then((r) => {
+		console.log(r)
+		console.log("got")
+		res.render('tabletest', {layout:false, table: r})
+	})
+	.fail((e) => {
+		res.send(e)
+	})
 })
 
 const port = 3000
--- sqlquery.js
+++ sqlquery.js
@@ -18,7 +18,7 @@ function getSqlTable(attribs, table) {
 }
 
 function getLocations() {
-	return module.exports.getSqlTable("*", "LOCATION")
+	return getSqlTable("*", "LOCATION")
 	.then(function(r){
 		return Promise.resolve(r)
 	})
@@ -27,19 +27,79 @@ function getLocations() {
 	})
 }
 
-function getRouteIDs(o) {
-    var a = 'aaa'
-    var query = `
-SELECT a.R_ID
-FROM HAS a, HAS b, LOCATION c, LOCATION d
-WHERE a.R_ID = b.R_ID
-AND c.L_ID=a.L_ID
-AND d.L_ID=b.L_ID
-AND c.L_Name = startLoc
-AND d.L_Name = endLoc
-AND a.TIME < b.TIME
+function exists(o) {
+	return (typeof o !== 'undefined' && o !== null && o !== '')
+}
+
+function getRouteIDsQuery(o) {
+var query = 
 `
+SELECT DISTINCT a.R_ID
+FROM HAS a, HAS b, LOCATION c, LOCATION d
+WHERE (a.R_ID = b.R_ID)
+AND (c.L_ID=a.L_ID)
+AND (d.L_ID=b.L_ID)
+${(false?'AND c.L_Name = startLoc':'')}
+${(false?'AND d.L_Name = endLoc':'')}
+AND (a.TIME < b.TIME)
+GROUP BY a.R_ID
+`
+
+return query;
+}
+
+function getRouteIDs(o) {
+
+    var query = getRouteIDsQuery(o)
+
+	return tp
+	.sql(query)
+	.execute()
+	.then((r) => {
+		return Promise.resolve(r)
+	})
+	.fail((e) => {
+		return Promise.reject(e)
+	})
     
 }
 
-module.exports = {getSqlTable, getLocations}
+function getResultsInRoute(o) {
+var query = 
+`
+SELECT r.R_ID, r.R_NAME, l.L_Name, h.TIME
+FROM ROUTE r, LOCATION l, HAS h
+WHERE (r.R_ID IN (${getRouteIDsQuery(o)}))
+AND (r.R_ID = h.R_ID)
+AND (l.L_ID = h.L_ID)
+ORDER BY r.R_ID, h.TIME
+`
+
+return query
+}
+
+function getDriverNames(admin) {
+var query = `
+SELECT FName${(admin ? ', LName' : '')}
+FROM DRIVER
+ORDER BY ${(admin ? 'Lname, ' : '')}FName
+`
+
+return tp
+	.sql(query)
+	.execute()
+	.then(function(results){
+		return Promise.resolve(results)
+	}) // follow up on the results
+	.fail(function(err) {
+		return Promise.reject(err)
+	}) // or do something if it errors
+}
+
+module.exports = {
+	getSqlTable,
+	getLocations,
+	getRouteIDs,
+	getResultsInRoute,
+	getDriverNames
+}
